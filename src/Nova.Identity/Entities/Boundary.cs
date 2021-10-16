5ed1521b748d79e808e45de0d612be2b425af4da
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-16 10:23:07 +0800

    It's a reset

--- src/Nova.Identity/Entities/Boundary.cs
+++
@@ -1,8 +0,0 @@
-namespace Nova.Identity.Entities;
-
-record Boundary
-{
-    public int Id { get; init; }
-    public string Name { get; init; } = "";
-    public string? LookupKey { get; init; }
-}
