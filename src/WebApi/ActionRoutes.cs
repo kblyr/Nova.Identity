ceb44dfca184f7494f56c86782de44ccb5efd388
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-19 07:51:36 +0800

    Added routing info

---
+++ src/WebApi/ActionRoutes.cs
@@ -0,0 +1,11 @@
+namespace Nova.Identity
+{
+    static class ActionRoutes
+    {
+        public static class Boundary
+        {
+            public const string Add = "add";
+            public const string Edit = "edit";
+        }
+    }
+}
---
+++ src/WebApi/ControllerRoutes.cs
@@ -0,0 +1,7 @@
+namespace Nova.Identity
+{
+    static class ControllerRoutes
+    {
+        public const string Boundary = "boundary";
+    }
+}
