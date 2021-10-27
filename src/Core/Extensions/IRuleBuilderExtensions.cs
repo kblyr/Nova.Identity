a55972c460bae0bab0952aca11cad99cb1735cdf
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-27 16:18:50 +0800

    Distinct Checks

--- src/Core/Extensions/IRuleBuilderExtensions.cs
+++ src/Core/Extensions/IRuleBuilderExtensions.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using FluentValidation;
 using Nova.Identity.Options;
 
@@ -63,17 +64,48 @@ namespace Nova.Identity
                 .WithMessage("'{PropertyName}' is invalid");
         }
 
-        public static IRuleBuilderOptions<T, IEnumerable<TProperty>> Distinct<T, TProperty>(this IRuleBuilder<T, IEnumerable<TProperty>> builder)
+        public static IRuleBuilderOptions<T, IEnumerable<TProperty>> Distinct<T, TProperty>(this IRuleBuilder<T, IEnumerable<TProperty>> builder, bool skipNull = true)
         {
-            return builder.Must(value => value is null || value.Count() == value.Distinct().Count())
+            var isDistinct = static (IEnumerable<TProperty> values, bool skipNull) => {
+                var hashSet = new HashSet<TProperty>();
+
+                foreach (var value in values)
+                {
+                    if (skipNull && value is null)
+                        continue;
+
+                    if (!hashSet.Add(value))
+                        return false;
+                }
+                
+                return true;
+            };
+
+            return builder.Must(value => value is null || isDistinct(value, skipNull))
                 .WithErrorCode(nameof(Distinct))
                 .WithMessage("'{PropertyName}' contains duplicate values");
         }
 
-        public static IRuleBuilderOptions<T, IEnumerable<TProperty>> Distinct<T, TProperty>(this IRuleBuilder<T, IEnumerable<TProperty>> builder, IEqualityComparer<TProperty> equalityComparer)
+        public static IRuleBuilderOptions<T, IEnumerable<TProperty>> DistinctBy<T, TProperty, TSubProperty>(this IRuleBuilder<T, IEnumerable<TProperty>> builder, Expression<Func<TProperty, TSubProperty>> subProperty, bool skipNull = true)
         {
-            return builder.Must(value => value is null || value.Count() == value.Distinct(equalityComparer).Count())
-                .WithMessage(nameof(Distinct))
+            var isDistinct = static (IEnumerable<TProperty> values, Expression<Func<TProperty, TSubProperty>> subProperty, bool skipNull) => {
+                var hashSet = new HashSet<TSubProperty>();
+                var getSubPropertyValue = subProperty.Compile();
+
+                foreach (var value in values)
+                {
+                    if (skipNull && value is null)
+                        continue;
+                    
+                    if (!hashSet.Add(getSubPropertyValue(value)))
+                        return false;
+                }
+                
+                return true;
+            };
+
+            return builder.Must(value => value is null || isDistinct(value, subProperty, skipNull))
+                .WithErrorCode(nameof(Distinct))
                 .WithMessage("'{PropertyName}' contains duplicate values");
         }
     }
